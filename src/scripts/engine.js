eb67cb0aaabc7e2255ca5d164ee64b7c6334a887
Mel Plens <melplan572@gmail.com>
2024-01-21 23:32:14 -0300

    enviando js pronto

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,28 +5,44 @@ const state = {
     view: {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelectorAll(".enemy"),
-        timeLeft: document.querySelectorAll("#time-left"),
+        timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"), // Como só tem um elemento com esse ID, sem o All.
     },
     values: {
-        TimeId: null,
         gameVelocity: 1000,
         hitPosition: 0, //onde o usuario clicou
         result: 0, //guarda a pontuacao geral
         curretTime: 60,
     },
+
+    actions: {
+        TimeId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
 };
 
 //para o time:
 
-function countDown(){
-    state.values.curretTime --; //decrementar
-
+function countDown() {
+    state.values.curretTime--; //decrementar
+    state.view.timeLeft.textContent = state.values.curretTime;
     //ver se o tempo acabou
-    if(state.values.curretTime<0){
-        alert ('Game Over ! O seu resultado foi :' + state.values.result);
+    if (state.values.curretTime < 0) {
+        alert('Game Over ! O seu resultado foi :' + state.values.result);
     }
 }
+//audio:
+function playSound() {
+    let audio = new Audio("./src/audios/soco.m4a");
+    audio.volume = 0.2;
+    audio.play();
+}
+
+
+
+
+
+
 
 //quadrado sorteado que ira aparecer o inimigo:
 function randomSquare() {
@@ -40,10 +56,11 @@ function randomSquare() {
     state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-    // a cada 1000 do gamevelocity segundos, ele vai executar a funcao random square
-    state.values.TimeId = setInterval(randomSquare, state.values.gameVelocity);
-}
+//simplifica nos actions em timeId.
+// function moveEnemy() {
+//     // a cada 1000 do gamevelocity segundos, ele vai executar a funcao random square
+//     state.values.TimeId = setInterval(randomSquare, state.values.gameVelocity);
+// }
 
 //Listener - conceito universal - ouvir uma acao, associa um evento para ser executado
 //Usuario interagir com o boneco:
@@ -54,6 +71,7 @@ function addListenerHitbox() {
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound();
 
             }
         });
@@ -64,7 +82,7 @@ function addListenerHitbox() {
 
 //inicio para chamar funcoes principais
 function initialize() {
-    moveEnemy();
+    // moveEnemy();
     addListenerHitbox();
 }
 
